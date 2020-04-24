50f1b5a454fc29901cba612a210ac1c6ee959158
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-04-24 11:36:23 -0700

    Added all the basics to get started.

---
+++ src/express.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const createApp = require("ringcentral-chatbot/dist/apps").default;
+
+const handle = async (event) => {
+  const { type, text, group, bot } = event;
+  if (type === "Message4Bot" && text === "ping") {
+    await bot.sendMessage(group.id, { text: "pong" });
+  }
+};
+const app = createApp(handle);
+app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT);
+
+setInterval(
+  async () =>
+    axios.put(
+      `${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`,
+      undefined,
+      {
+        auth: {
+          username: process.env.RINGCENTRAL_CHATBOT_ADMIN_USERNAME,
+          password: process.env.RINGCENTRAL_CHATBOT_ADMIN_PASSWORD,
+        },
+      }
+    ),
+  24 * 60 * 60 * 1000
+);
