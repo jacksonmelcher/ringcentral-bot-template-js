5eb7d5e329ce5496f503d6c20f396118cd8b5173
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-05-14 13:14:09 -0700

    Fixed import statements

--- src/express.js
+++ src/express.js
@@ -1,26 +1,16 @@
-const axios = require("axios");
-const createApp = require("ringcentral-chatbot/dist/apps").default;
+import axios from 'axios';
+import createApp from 'ringcentral-chatbot/dist/apps';
 
 const handle = async (event) => {
-  const { type, text, group, bot } = event;
-  if (type === "Message4Bot" && text === "ping") {
-    await bot.sendMessage(group.id, { text: "pong" });
-  }
+    const { type, text, group, bot } = event;
+    if (type === 'Message4Bot' && text === 'ping') {
+        await bot.sendMessage(group.id, { text: 'pong' });
+    }
 };
 const app = createApp(handle);
 app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT);
 
 setInterval(
-  async () =>
-    axios.put(
-      `${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`,
-      undefined,
-      {
-        auth: {
-          username: process.env.RINGCENTRAL_CHATBOT_ADMIN_USERNAME,
-          password: process.env.RINGCENTRAL_CHATBOT_ADMIN_PASSWORD,
-        },
-      }
-    ),
-  24 * 60 * 60 * 1000
+    () => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`),
+    86400000
 );
