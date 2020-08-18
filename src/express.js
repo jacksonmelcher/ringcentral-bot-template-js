81a074418649827ac3604f1084a6aa2157deaf2b
Jackson Melcher <jackson.melcher@ringcentral.com>
2020-08-18 16:35:55 -0700

    Added log4js

--- src/express.js
+++ src/express.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 import createApp from 'ringcentral-chatbot/dist/apps';
 import eventHandler from './eventHandler';
+import log4js from 'log4js';
 
+log4js.configure({
+    appenders: { out: { type: 'stdout' } },
+    categories: { default: { appenders: ['out'], level: 'trace' } },
+});
+const logger = log4js.getLogger('EXPRESS');
 const handle = async (event) => {
     await eventHandler(event);
 };
 const app = createApp(handle);
-app.listen(process.env.PORT || 3000, () => console.log('Server is running...'));
+
+app.listen(process.env.PORT || 3000, () => logger.info('Server is running...'));
 
 setInterval(
     () => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`),
